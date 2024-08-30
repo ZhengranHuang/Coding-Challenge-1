1375030a16c2e1ad05b9fbf65df2dfd50a62eed2
ZhengranHuang <Zhengran@usf.edu>
2024-08-30 14:56:23 -0400

    Task 1: Variables and Data Types

---
+++ cc_1.js
@@ -0,0 +1,9 @@
+// Task 1: Variables and Data Types
+
+let employeeName = "Trump"
+const employeeID = 1;
+var isActive = true;
+
+console. log(employeeName, typeof(employeeName));
+console. log(employeeID, typeof(employeeID));
+console. log(isActive, typeof(isActive));
